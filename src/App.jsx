d36d650291b44798cba70759611861472d312ed8
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2022-05-17 19:00:02 +0800

    initial homepage

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+import {Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+  );
+}
+
+export default App;
